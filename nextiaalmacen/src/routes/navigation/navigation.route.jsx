45044cb73d16b86a8a75927c28fe8d0b893f5d46
JordyCA <jordycastroavendano@gmail.com>
2023-01-29 14:36:53 -0600

    Modify visual - navigation

--- nextiaalmacen/src/routes/navigation/navigation.route.jsx
+++ nextiaalmacen/src/routes/navigation/navigation.route.jsx
@@ -1,21 +1,38 @@
 //** Libraries */
-import { Fragment } from 'react';
+import {
+    Fragment,
+    useContext
+} from 'react';
 import { Outlet } from 'react-router-dom';
 //** Boostrap Libraries */
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+//** Contexts */
+import { UserContext } from '../../contexts/user/user.context';
 //**Components */
 import NavBar from '../../components/navbar/navbar.component';
 
 const Navigation = () => {
+    const { userToken } = useContext(UserContext);
     return (
         <Fragment>
             <Container>
-                <Row>
-                    <Col sm={4}><NavBar /></Col>
-                    <Col sm={8} className="d-flex align-items-center"><Outlet/></Col>
-                </Row>
+                
+                    {
+                        userToken !== null ? (
+                            <Row>
+                            <Col sm={4}><NavBar /></Col>
+                            <Col sm={8} className="d-flex align-items-center"><Outlet /></Col>
+                            </Row>
+                        ) : (
+                            <Row>
+                                <Col className="d-flex align-items-center"><Outlet /></Col>
+                            </Row>
+                        )
+                    }
+                
+
             </Container>
         </Fragment>
     );
